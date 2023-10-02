e9202c7bb15dfe96da9392a2550836674524fb8e
Aryan <aryancr72000@gmail.com>
2023-10-03 03:58:19 +0530

    completed the project added gen column aswell

--- Flappy_bird.py
+++ Flappy_bird.py
@@ -5,6 +5,8 @@ import os
 import random
 pygame.font.init()
 
+GEN=0
+
 WIN_WIDTH=500
 WIN_HEIGHT=800
 
@@ -145,20 +147,37 @@ class Base:
         win.blit(self.IMG,(self.x1,self.y))
         win.blit(self.IMG,(self.x2,self.y))
 
-def draw_window(win,bird,pipes,base,score):
+def draw_window(win,birds,pipes,base,score,gen):
     win.blit(BG_IMG,(0,0))
     for pipe in pipes:
         pipe.draw(win)
     text=STAT_FONT.render("Score: " +str(score),1,(255,255,255))
     win.blit(text,(WIN_WIDTH-10-text.get_width(),10))
+    text=STAT_FONT.render("Gen: " +str(gen),1,(255,255,255))
+    win.blit(text,(10,10))
+
     base.draw(win)
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
     pygame.display.update()
 
-def main():
-    bird=Bird(230,350)
+def main(genomes,config):
+    global GEN
+    GEN+=1
+    nets=[]
+    ge=[]
+    birds=[]
+
+    for _,g in genomes:
+        net=neat.nn.FeedForwardNetwork.create(g,config)
+        nets.append(net)
+        birds.append(Bird(230,350))
+        g.fitness=0
+        ge.append(g)
+
+
     base=Base(730)
-    pipes=[Pipe(700)]
+    pipes=[Pipe(600)]
     win=pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
     clock=pygame.time.Clock()
     score=0
@@ -167,44 +186,80 @@ def main():
     while run:
 
 
-        clock.tick(40)
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
-        #bird.move()
+                pygame.quit()
+                quit()
+
+        pipe_ind=0
+        if len(birds)>0:
+            if len(pipes)>1 and birds[0].x>pipes[0].PIPE_TOP.get_width():
+                pipe_ind=1
+        else:
+            run=False
+            break
+        
+        for x,bird in enumerate(birds):
+            ge[x].fitness+=0.07
+            bird.move()
+            
+
+            output=nets[birds.index(bird)].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+            if output[0]>0.5:
+                bird.jump()
+
         add_pipe=False
         rem=[]
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass
+            for x,bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness-=2
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+            
+                if not pipe.passed and pipe.x+pipe.PIPE_TOP.get_width()<bird.x:
+                    pipe.passed=True
+                    add_pipe=True
             if pipe.x+pipe.PIPE_TOP.get_width()<0:
                 rem.append(pipe)
-            if not pipe.passed and pipe.x<bird.x:
-                pipe.passed=True
-                add_pipe=True
-            
             pipe.move()
         if add_pipe:
             score+=1
+            
+            for g in ge:
+                g.fitness+=10
             pipes.append(Pipe(600))
         
         for r in rem:
+            
             pipes.remove(r)
-        rem=[]
-
-        if bird.y+bird.img.get_height()>=730:
-            pass
+        
+        for x,bird in enumerate(birds):
+            if bird.y+bird.img.get_height()>=730 or bird.y<0:
+                ge[x].fitness-=2
+                birds.pop(x)
+                ge.pop(x)
+                nets.pop(x)
 
 
         base.move()
-        draw_window(win,bird,pipes,base,score)
-    pygame.quit()
-    quit()
+        draw_window(win,birds,pipes,base,score,GEN)
+    
 
-main()
 
 def run(config_path):
-    pass
+    config=neat.Config(neat.DefaultGenome,neat.DefaultReproduction,neat.DefaultSpeciesSet,neat.DefaultStagnation,config_path)
+    p=neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats=neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner=p.run(main,50)
+    print("winner  --",winner)
+
 
 if __name__=="__main__":
     local_dir=os.path.dirname(__file__)
